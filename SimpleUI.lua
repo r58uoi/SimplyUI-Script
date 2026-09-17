396cc2028142121220213e3baecf561c5cca04ce
r58uoi <gertsmatvei@gmail.com>
2026-09-17 07:40:00 +0500

    Update SimpleUI.lua

--- SimpleUI.lua
+++ SimpleUI.lua
@@ -395,46 +395,6 @@ function SimplyUI:CreateColorSystem(Object)
 		UI_GUI_ColorSystem["FrameDragTwo"].Changed:Connect(UpdateDrag)
 		UI_GUI_ColorSystem["FrameDragThree"].Changed:Connect(UpdateDrag)
 
-		local UIS = game:GetService('UserInputService')
-		local frame = UI_GUI_ColorSystem["FrameBoxColorRGB"]
-		local GUI = UI_GUI_ColorSystem["ColorFrame"]
-		local dragSpeed = 0.001
-
-		local dragToggle = nil
-		local dragStart = nil
-		local startPos = nil
-
-		local function updateInput(input)
-			local delta = input.Position - dragStart
-			local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
-				startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-			game:GetService('TweenService'):Create(GUI, TweenInfo.new(dragSpeed), {Position = position}):Play()
-		end
-
-
-
-		frame.InputBegan:Connect(function(input)
-			if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
-				dragToggle = true
-				dragStart = input.Position
-				startPos = GUI.Position
-				input.Changed:Connect(function()
-					if input.UserInputState == Enum.UserInputState.End then
-						dragToggle = false
-					end
-				end)
-			end
-		end)
-
-
-		UIS.InputChanged:Connect(function(input)
-			if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-				if dragToggle then
-					updateInput(input)
-				end
-			end
-		end)
-
 		return {
 			UI_GUI_ColorSystem["ColorFrame"],
 			UI_GUI_ColorSystem["FrameBoxColorRGB"],
